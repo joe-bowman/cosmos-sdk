3bf163880382627feeda084028a61f232833107c
Joe Bowman <joe@chorus.one>
2018-12-18 06:07:01 +0000

    remove async calls

--- x/distribution/client/rest/tx.go
+++ x/distribution/client/rest/tx.go
@@ -24,18 +24,15 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec, 
 type WithdrawDelegatorReq struct {
 	BaseReq           utils.BaseReq    `json:"base_req"`
 	OnlyFromValidator string           `json:"only_from_validator"`
-	Async             bool             `json:"async"`
 }
 
 type WithdrawValidatorReq struct {
 	BaseReq      utils.BaseReq  `json:"base_req"`
-	Async        bool           `json:"async"`
 }
 
 type SetWithdrawAddressReq struct {
 	BaseReq      utils.BaseReq  `json:"base_req"`
 	WithdrawAddr string         `json:"withdraw_addr"` // bech32 Address of the account to withdraw to
-	Async        bool           `json:"async"`
 }
 
 func withdrawHandlerFn(cdc *codec.Codec, kb keys.Keybase, cliCtx context.CLIContext) http.HandlerFunc {
@@ -59,13 +56,8 @@ func withdrawHandlerFn(cdc *codec.Codec, kb keys.Keybase, cliCtx context.CLICont
     }
 
     //var msg sdk.Msg
-
     valAddr := sdk.ValAddress(info.GetAddress())
     msg := types.NewMsgWithdrawValidatorRewardsAll(valAddr)
-
-		if req.Async == true {
-		  cliCtx.Async = true
-		}
 
     // build and sign the transaction, then broadcast to Tendermint
     utils.CompleteAndBroadcastTxREST(w, r, cliCtx, baseReq, []sdk.Msg{msg}, cdc)
@@ -109,10 +101,6 @@ func withdrawHandlerDelegatorFn(cdc *codec.Codec, kb keys.Keybase, cliCtx contex
     }
 
     // build and sign the transaction, then broadcast to Tendermint
-		if req.Async == true {
-		  cliCtx.Async = true
-		}
-
 		utils.CompleteAndBroadcastTxREST(w, r, cliCtx, baseReq, []sdk.Msg{msg}, cdc)
   }
 }
@@ -153,11 +141,7 @@ func setWithdrawAddressHandlerFn(cdc *codec.Codec, kb keys.Keybase, cliCtx conte
 
     msg := types.NewMsgSetWithdrawAddress(withdrawAddr, valAddr)
 
-		if req.Async == true {
-		  cliCtx.Async = true
-		}
-
-    // build and sign the transaction, then broadcast to Tendermint
+		// build and sign the transaction, then broadcast to Tendermint
     utils.CompleteAndBroadcastTxREST(w, r, cliCtx, baseReq, []sdk.Msg{msg}, cdc)
   }
 }
