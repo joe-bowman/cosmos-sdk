5c9ab3dfaba6e90f85cd9be962b6f5de4dd0e708
Joe Bowman <joe@chorus.one>
2020-02-10 17:50:30 +0000

    use infiniteGasMeter for iterating delegators/delegations

--- x/staking/keeper/slash.go
+++ x/staking/keeper/slash.go
@@ -121,7 +121,9 @@ func (k Keeper) Slash(ctx sdk.Context, consAddr sdk.ConsAddress, infractionHeigh
 
 	// we want to export all delegations for every user who was slashed.
 	affectedDelegators := make(map[string]sdk.AccAddress)
-	for _, delegation := range k.GetValidatorDelegations(ctx, validator.GetOperator()) {
+	noGasCtx, _ := ctx.CacheContext()
+	noGasCtx.WithGasMeter(sdk.NewInfiniteGasMeter()).WithBlockGasMeter(sdk.NewInfiniteGasMeter())
+	for _, delegation := range k.GetValidatorDelegations(noGasCtx, validator.GetOperator()) {
 		affectedDelegators[delegation.GetDelegatorAddr().String()] = delegation.GetDelegatorAddr()
 	}
 	for _, delegator := range affectedDelegators {
